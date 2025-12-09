e181acfd722645229ebe867883ba801e80eb4229
johnbendelac <johnbendelac@gmail.com>
2025-12-09 15:41:36 +0200

    Connect frontend to Render backend

--- src/app/propriete/[id]/page.tsx
+++ src/app/propriete/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "next/navigation"
 
+
 // const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:3001"
 const API_BASE = "https://site-immo-backend.onrender.com"
 
